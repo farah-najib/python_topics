c6adacba779e010ccbbd3e32d5532425d90dda35
Farah Najib <farahengnajib@gmail.com>
2024-04-09 11:43:13 +0200

    first

---
+++ index_operator.py
@@ -0,0 +1,14 @@
+a="hello students" #Characters are indexed left to right from position ‘0’ to position ‘13’. Positions are also named from right to left using negative numbers, where -1 is the rightmost index and so on.
+b=['hello','pi','value','is',3.14]
+c=('hello','pi','value','is',3.14)
+l=a[2]
+print(l)
+lastcher=a[-1]
+print(lastcher)
+print(b[2])
+print(b[3-6])
+print(b[-2])
+print(c[0])
+print(c[-1])
+print(c[2-4])
+print(b[7-8])
